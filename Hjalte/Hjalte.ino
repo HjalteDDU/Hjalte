8001583f161d1a874dcffd9838a46e72f49c1634
Hjalte Vilford Vinther <hjal0320@skivecollege.dk>
2019-08-19 13:43:39 +0200

    Add project files.

---
+++ Hjalte/Hjalte.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Hjalte.ino
+ Created:	8/19/2019 1:43:22 PM
+ Author:	Hjalte
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
