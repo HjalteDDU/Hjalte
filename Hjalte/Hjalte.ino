f98045225bd328322f6b23ae90a151ff7eeee340
Hjalte Vilford Vinther <hjal0320@skivecollege.dk>
2019-08-19 13:46:20 +0200

    Added Hello World

--- Hjalte/Hjalte.ino
+++ Hjalte/Hjalte.ino
@@ -6,10 +6,10 @@
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-
+	//Hello world
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-  
+ 
 }
